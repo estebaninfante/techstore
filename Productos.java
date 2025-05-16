33c0c3b32afe2274f5f509970458858170485834
Gabo206 <gaboromandini@gmail.com>
2025-05-16 10:54:24 -0500

    Gil

--- Productos.java
+++ Productos.java
@@ -1,9 +1,26 @@
-class Producto {
+class Productos {
 
   private String nombre;
-  private Int stock;
-  private String descripción;
-  private String referencia;
+  private Double precio;
+  private int stock;
+  private String descripcion;
 
-  public Producto(nombre, stock, descripción,  )
+  public Productos(String nombre, int stock, Double precio, String descripcion) {
+    this.nombre = nombre;
+    this.stock = stock;
+    this.precio = precio;
+    this.descripcion = descripcion;
+  }
+  public String conseguirnombre () {
+    return nombre;
+  }
+  public Double conseguirprecio () {
+    return precio;
+  }
+  public int conseguirstock () {
+    return stock;
+  }
+  public String conseguirdescripcion () {
+    return descripcion;
+  }
 }
