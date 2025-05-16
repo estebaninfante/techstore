428a0555ce89b7b9491817a0305c06b422febed5
estebaninfante <juanesteban.infantem@gmail.com>
2025-05-16 11:25:03 -0500

    Añadir Producto añadido

--- Inventario.java
+++ Inventario.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 class Inventario{
   private int cantidadProductos;
@@ -11,8 +12,15 @@ class Inventario{
     this.productos = new ArrayList<Producto>();
   }
  
-  public static void AñadirProducto(){
+  public void AñadirProducto(){
+    String nombre = JOptionPane.showInputDialog(null, "Nombre del producto");
+    String descripcion = JOptionPane.showInputDialog(null, "Añade el precio del producto: ");
+    double precio = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresa el precio del producto"));
+    int stock = Integer.parseInt(
+      JOptionPane.showInputDialog(null, "Ingresa la cantidad de producto que tienes.")
+    );
     
-    nuevoProducto = Producto();
+    Producto nuevoProducto = new Producto(nombre, stock, precio, descripcion);
+    productos.add(nuevoProducto);
   } 
 }
